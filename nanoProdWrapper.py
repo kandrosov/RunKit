f30a4086167dcbc8cb71100cf5b5f3be87c0045d
Irene Andreou <irandreo@cern.ch>
2023-07-19 12:31:22 +0200

    modifying global tags

--- nanoProdWrapper.py
+++ nanoProdWrapper.py
@@ -46,8 +46,8 @@ cond_mc = {
   'Run2_2016': 'auto:run2_mc',
   'Run2_2017': 'auto:phase1_2017_realistic',
   'Run2_2018': 'auto:phase1_2018_realistic',
-  'Run3_2022': '126X_mcRun3_2022_realistic_v2',
-  'Run3_2022EE': '126X_mcRun3_2022_realistic_postEE_v1',
+  'Run3_2022': '124X_mcRun3_2022_realistic_v12',
+  'Run3_2022_postEE': '124X_mcRun3_2022_realistic_postEE_v1',
 }
 
 if options.era.startswith('Run2'):
@@ -56,8 +56,8 @@ if options.era.startswith('Run2'):
   era_mod = ',run2_nanoAOD_106Xv2'
 elif options.era.startswith('Run3'):
   cond_data_run3 = {
-    'Run3_2022CDE': '124X_dataRun3_v14',
-    'Run3_2022FG': '124X_dataRun3_Prompt_v10',
+    'Run3_2022': '124X_dataRun3_PromptAnalysis_v1', # will eventually change this to the ReReco tag
+    'Run3_2022_postEE': '124X_dataRun3_Prompt_v10',
   }
   if options.sampleType == 'data':
     cond_data = cond_data_run3[options.era]
